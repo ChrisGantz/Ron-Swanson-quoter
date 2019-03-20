8341c09a09db6029bee92c4aa1fff3a40be31c0f
Chris Gantz <chris.gonzalez836@gmail.com>
2019-03-19 17:04:39 -0700

    removed template files from create react app

---
+++ src/components/giftro.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Giftro = () => {
+  return (
+    <div className="gif_wrap">
+      {/* <iframe src="https://giphy.com/embed/Y7SeP7arJWBZ6" width="480" height="270" frameBorder="0" className="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/Y7SeP7arJWBZ6" target="_blank" rel="noopener noreferrer">via giphy</a></p> */}
+      <iframe src="https://giphy.com/embed/TqM6POtVeOW6Q" width="480" height="268" frameBorder="0" className="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/words-ron-swanson-TqM6POtVeOW6Q" target="_blank" rel="noopener noreferrer">via giphy</a></p>
+    </div>
+  );
+};
+
+export default Giftro;
